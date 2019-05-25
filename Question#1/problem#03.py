c371127d1f43674c7f81a2838c4734cec7d20eaa
ShanmukhaSrinivas <vemulasrinivas2505@gmail.com>
2019-05-25 19:24:09 +0530

    problem#3

--- Question#1/problem#03.py
+++ Question#1/problem#03.py
@@ -1,2 +1,25 @@
+#This program shows the diffrence b/w normal python program and numba
+
 import quantecon as qe
+from numba import jit
+from array import *
+def l():
+    arr = array('i',[10,11,12,13,14])
+    arr[1] = 80
+    for x in arr:
+        print(x)
+
+@jit
+def k():
+    arr = array('i',[10,11,12,13,14])
+    arr[1] = 80
+    for x in arr:
+        print(x)
+
 qe.util.tic()
+l()
+p1 = qe.util.toc()
+qe.util.tic()
+k()
+p2 = qe.util.toc()
+print(p1/p2)
